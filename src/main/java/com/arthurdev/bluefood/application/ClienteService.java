d0b58115fb94a9173a7ac6fc7d5392b8e783eb70
arthur <arthurkenned21@gmail.com>
2021-02-23 01:14:56 -0300

    aula-12.18-> validaçao de negocio versao 2

--- src/main/java/com/arthurdev/bluefood/application/ClienteService.java
+++ src/main/java/com/arthurdev/bluefood/application/ClienteService.java
@@ -8,22 +8,35 @@ import com.arthurdev.bluefood.domain.cliente.ClienteRepository;
 
 @Service
 public class ClienteService {
-    
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
-	
-	
+
 	public void saveCliente(Cliente cliente) throws ValidatrionException {
-		
-		if(! validateEmail(cliente.getEmail(), cliente.getId())) {
+
+		if (!validateEmail(cliente.getEmail(), cliente.getId())) {
 			throw new ValidatrionException("o email esta duplicado");
 		}
-		
+
 		clienteRepository.save(cliente);
 	}
-	
-	private boolean validateEmail(String email , Integer id) {
-		return false;
+
+	private boolean validateEmail(String email, Integer id) {
+
+		Cliente clienteDB = clienteRepository.findByEmail(email);					 // busca cliente pelo email
+
+		if (clienteDB != null) { 													// se nao encontrar o cliente no bd , entao vc esta criando um novo , pode retornar true
+																					
+			if (id == null) { 														// se o id que eu estou passando como parametro é null , siguinifica que estou inserindo , mas tem algem como o mesmo email , entao o email esta duplicado
+																					
+				return false;
+			}
+
+			if (!clienteDB.getId().equals(id)) { 									// o id nao esta nulo , vc esta alterando um regirtro pra um email que ja existe entao retorna false
+				return false;
+			}
+
+		}
+		return true;
 	}
 }

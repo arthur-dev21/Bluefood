c61bf1b0ce1000c8c7f235b5285671448b56ca1b
arthur <arthurkenned21@gmail.com>
2021-02-28 14:42:47 -0300

    aula13.14 -> mosttrando imagens ce caetoria no

--- src/main/java/com/arthurdev/bluefood/application/service/ImageService.java
+++ src/main/java/com/arthurdev/bluefood/application/service/ImageService.java
@@ -1,6 +1,7 @@
 package com.arthurdev.bluefood.application.service;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -8,18 +9,51 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.arthurdev.bluefood.util.IOUtils;
 
+
+
 @Service
 public class ImageService {
 	
 	
 	@Value("${bluefood.files.logotipo}")
 	private String logotiposDir;
+	
+	@Value("${bluefood.files.categoria}")
+	private String categoriasDir;
+	
+	@Value("${bluefood.files.comida}")
+	private String comidasDir;
     
 	public void uploadLogotipo(MultipartFile multipartFile, String fileName) {
 		try {
 			IOUtils.copy(multipartFile.getInputStream(), fileName, logotiposDir);
 		} catch (IOException e) {
 			throw new ApplicationServiceException(e);
+		}
+	}
+	
+public byte[] getBytes(String type, String imgName) {
+		
+		try {
+			String dir;
+			
+			if ("comida".equals(type)) {
+				dir = comidasDir;
+			
+			} else if ("logotipo".equals(type)) {
+				dir = logotiposDir;
+			
+			} else if ("categoria".equals(type)) {
+				dir = categoriasDir;
+			
+			} else {
+				throw new Exception(type + " não é um tipo de imagem válido");
+			}
+			
+			return IOUtils.getBytes(Paths.get(dir, imgName));
+		
+		} catch (Exception e) {
+			throw new ApplicationServiceException(e);                   //como é um serviço tem que ser esse tipo de excessaaao
 		}
 	}
 }
--- src/main/java/com/arthurdev/bluefood/infrastructure/web/controller/ClienteController.java
+++ src/main/java/com/arthurdev/bluefood/infrastructure/web/controller/ClienteController.java
@@ -1,9 +1,11 @@
 package com.arthurdev.bluefood.infrastructure.web.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,7 +18,11 @@ import com.arthurdev.bluefood.application.service.ClienteService;
 import com.arthurdev.bluefood.application.service.ValidationException;
 import com.arthurdev.bluefood.domain.cliente.Cliente;
 import com.arthurdev.bluefood.domain.cliente.ClienteRepository;
+import com.arthurdev.bluefood.domain.restaurante.CategoriaRestaurante;
+import com.arthurdev.bluefood.domain.restaurante.CategoriaRestauranteRepository;
 import com.arthurdev.bluefood.util.SecurityUtils;
+
+
 
 
 
@@ -32,10 +38,15 @@ public class ClienteController {
 	private ClienteRepository clienteRepository;
 	
 	@Autowired
+	private CategoriaRestauranteRepository categoriaRestauranteRepository;
+	
+	@Autowired
 	private ClienteService clienteService;
     
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		List<CategoriaRestaurante> categorias = categoriaRestauranteRepository.findAll(Sort.by("nome"));
+		model.addAttribute("categorias", categorias);
 		return "cliente-home";
 	}
 	
